f099b6192c1245ff0f6312b4720db735d4901807
IgnacioPena21 <ignacio.penasepulveda@elev.ga.ntig.se>
2023-12-17 22:37:30 +0100

    .

--- js/AAscript.js
+++ js/AAscript.js
@@ -1,4 +1,10 @@
-
-fetch('https://api-football-v1.p.rapidapi.com/c28506c129msh794c084428304a7p1b5d2fjsnd7640ec146f0')
+fetch('https://thesportsdb.com/api/v1/json/3/all_sports.php?id=%22102%22')
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => {
+    console.log(data.sports[0]);
+
+    function Explanation(Futinfo){
+        document.getElementById("FutInfo").InnerHTML = `Information about the Football:<br>${Futinfo}`;
+    }
+    Explanation(data.sports[0].strSportDescription);
+} )
